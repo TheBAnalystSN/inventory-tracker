b2a6d35df068efd6e2bade3cb751fab772f5db01
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2025-11-13 21:34:34 -0500

    Compile the TypeScript code

--- inventory-tracker/src/models/Product.d.ts
+++ inventory-tracker/src/models/Product.d.ts
@@ -1,2 +1,9 @@
-export {};
+export declare abstract class Product {
+    protected sku: string;
+    protected name: string;
+    protected price: number;
+    constructor(sku: string, name: string, price: number);
+    displayDetails(): string;
+    abstract getPriceWithTax(): number;
+}
 //# sourceMappingURL=Product.d.ts.map
--- inventory-tracker/src/models/Product.js
+++ inventory-tracker/src/models/Product.js
@@ -1,3 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = void 0;
+// The Product Base Class
+class Product {
+    sku;
+    name;
+    price;
+    constructor(sku, name, price) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+    }
+    displayDetails() {
+        return `SKU: ${this.sku} | Name: ${this.name} | Price: $${this.price.toFixed(2)}`;
+    }
+}
+exports.Product = Product;
 //# sourceMappingURL=Product.js.map
