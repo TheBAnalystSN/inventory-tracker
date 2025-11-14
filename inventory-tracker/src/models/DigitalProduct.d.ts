326e165f62aa1d2553ccb6c0ad15d10668b3df78
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2025-11-13 21:47:18 -0500

    Compiled the TypeScript code

--- inventory-tracker/src/models/DigitalProduct.d.ts
+++ inventory-tracker/src/models/DigitalProduct.d.ts
@@ -1,2 +1,9 @@
-export {};
+import { Product } from "./Product";
+export declare class DigitalProduct extends Product {
+    private fileSize;
+    constructor(sku: string, name: string, price: number, fileSize: number);
+    getPriceWithTax(): number;
+    get formattedFileSize(): string;
+    displayDetails(): string;
+}
 //# sourceMappingURL=DigitalProduct.d.ts.map
