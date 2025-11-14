1446f6f00f49977439273960ef77de208d7d8aeb
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2025-11-13 21:32:43 -0500

    Created the Product Base Class

---
+++ inventory-tracker/src/models/Product.ts
@@ -0,0 +1,14 @@
+// The Product Base Class
+export abstract class Product {
+  constructor(
+    protected sku: string,
+    protected name: string,
+    protected price: number
+  ) {}
+
+  displayDetails(): string {
+    return `SKU: ${this.sku} | Name: ${this.name} | Price: $${this.price.toFixed(2)}`;
+  }
+
+  abstract getPriceWithTax(): number;
+}
